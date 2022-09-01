ca48f9be9598a41eb28f447ecde94ba5c0f6c597
Theo Vora <theo.vora@gmail.com>
2022-09-01 19:25:01 -0400

    make a stackview of 3 elements

--- ToggleStackViewElements/ViewController.swift
+++ ToggleStackViewElements/ViewController.swift
@@ -9,29 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var helloLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Hello world"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.layer.borderColor = UIColor(ciColor: .red).cgColor
-        label.layer.borderWidth = 1.0
-        return label
+    lazy var vStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.applyBorder(.cyan)
+        stack.distribution = .fillEqually
+        return stack
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
-        view.addSubview(helloLabel)
+        applyAttributes()
+        applyConstraints()
+        applyColors()
+    }
+    
+    func label(text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.applyBorder()
+        return label
+    }
+    
+    private func applyAttributes() {
+        view.addSubview(vStack)
+        vStack.addArrangedSubview(label(text: "hello there"))
+        vStack.addArrangedSubview(label(text: "come here"))
+        vStack.addArrangedSubview(label(text: "my little friend"))
         
         view.translatesAutoresizingMaskIntoConstraints = false
-        
+    }
+    
+    private func applyConstraints() {
         NSLayoutConstraint.activate([
-            helloLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            helloLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            helloLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            helloLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            vStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+            vStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 52),
+            vStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+            vStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -52),
         ])
+    }
+    
+    private func applyColors() {
+        view.backgroundColor = .white
     }
 }
 
+extension UIView {
+    func applyBorder(_ color: CIColor = .red, border: CGFloat = 1.0) {
+        self.layer.borderColor = UIColor(ciColor: color).cgColor
+        self.layer.borderWidth = border
+    }
+}
