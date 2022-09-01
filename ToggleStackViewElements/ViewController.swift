3120cfe8e73d0fe71932dec319f404ada670b4dc
Theo Vora <theo.vora@gmail.com>
2022-09-01 19:03:55 -0400

    make UIKit programmatic, throw out storyboard

--- ToggleStackViewElements/ViewController.swift
+++ ToggleStackViewElements/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var helloLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello world"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.borderColor = UIColor(ciColor: .red).cgColor
+        label.layer.borderWidth = 1.0
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        view.addSubview(helloLabel)
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            helloLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            helloLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            helloLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            helloLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
-
-
 }
 
