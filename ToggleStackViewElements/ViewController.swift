6c60157a9ae6defa04c6226beb5b8e1bf16eda9f
Theo Vora <theo.vora@gmail.com>
2022-09-06 12:19:09 -0400

    utilize button to toggle the first element on and off

--- ToggleStackViewElements/ViewController.swift
+++ ToggleStackViewElements/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var showHelloLabel: Bool = true {
+        didSet {
+            helloLabel.isHidden = !showHelloLabel
+        }
+    }
+    
     lazy var vStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
@@ -21,6 +27,19 @@ class ViewController: UIViewController {
     lazy var helloLabel: UILabel = {
         let label = label(text: "hi there")
         return label
+    }()
+    
+    lazy var toggleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("button", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.applyBorder(.blue)
+        button.backgroundColor = .white
+        button.isUserInteractionEnabled = true
+        button.addTarget(self, action: #selector(toggleButtonTapped), for: .touchUpInside)
+        print("button created")
+        return button
     }()
     
     override func viewDidLoad() {
@@ -41,6 +60,7 @@ class ViewController: UIViewController {
     
     private func applyAttributes() {
         view.addSubview(vStack)
+        view.addSubview(toggleButton)
         vStack.addArrangedSubview(helloLabel)
         vStack.addArrangedSubview(label(text: "come here"))
         vStack.addArrangedSubview(label(text: "my little friend"))
@@ -48,11 +68,15 @@ class ViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         
 //        helloLabel.isEnabled = false // just grays it out
-        helloLabel.isHidden = true // this works. and the stack view adjusts to fill the space.
+//        helloLabel.isHidden = true // this works. and the stack view adjusts to fill the space.
+        showHelloLabel = true
     }
     
     private func applyConstraints() {
         NSLayoutConstraint.activate([
+            toggleButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 52),
+            toggleButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            toggleButton.widthAnchor.constraint(equalToConstant: 120),
             vStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
             vStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 52),
             vStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
@@ -62,6 +86,10 @@ class ViewController: UIViewController {
     
     private func applyColors() {
         view.backgroundColor = .white
+    }
+    
+    @objc private func toggleButtonTapped() {
+        showHelloLabel.toggle()
     }
 }
 
